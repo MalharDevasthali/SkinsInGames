6eca536541a1bbed63312174854cd32007a6591c
Malhar Devasthali <malhardevasthali007@gmail.com>
2021-10-09 13:12:51 +0530

    #Removed Cinemachine

--- Skins In Games/Assets/Scripts/Camera/CameraController.cs
+++ Skins In Games/Assets/Scripts/Camera/CameraController.cs
@@ -1,20 +1,34 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿
 using UnityEngine;
 using Commons;
-using Cinemachine;
+//using Cinemachine;
 
 public class CameraController : GenericMonoSingleton<CameraController>
 {
-    private CinemachineVirtualCamera virtualCamera;
+    // private CinemachineVirtualCamera virtualCamera;
+    private Transform target;
+    [SerializeField] private float smoothSpeed = 0.125f;
+    [SerializeField] private Vector3 offset;
+    private bool hasTargetSet = false;
 
     protected override void Awake()
     {
         base.Awake();
-        virtualCamera = GetComponent<CinemachineVirtualCamera>();
+        // virtualCamera = GetComponent<CinemachineVirtualCamera>();
     }
-    public void SetTarget(Transform taregt)
+    public void SetTarget(Transform _taregt)
     {
-        virtualCamera.Follow = taregt;
+        //  virtualCamera.Follow = taregt;
+        target = _taregt;
+        hasTargetSet = true;
+    }
+    private void FixedUpdate()
+    {
+        if (hasTargetSet && target != null)
+        {
+            Vector3 disiredPosition = target.position + offset;
+            Vector3 smoothedPosition = Vector3.Lerp(transform.position, disiredPosition, smoothSpeed * Time.deltaTime);
+            transform.position = smoothedPosition;
+        }
     }
 }
